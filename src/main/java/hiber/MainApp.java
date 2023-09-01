bd54d31106e1151b9149e82556ff235d59e7ded8
Andy72 <39372152+kirizleev@users.noreply.github.com>
2023-09-01 15:37:46 +0500

    task solved

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,57 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.CarService;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+      CarService carService = context.getBean(CarService.class);
+
+      User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+      Car car1 = new Car("BMW", 3);
+      user1.addCar(car1);
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      Car car2 = new Car("Nissan", 350);
+      user2.addCar(car2);
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      Car car3 = new Car("Mercedes", 500);
+      user3.addCar(car3);
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+      Car car4 = new Car("Lada", 7);
+      user4.addCar(car4);
+
+      userService.add(user1);
+      carService.add(car1);
+      userService.add(user2);
+      carService.add(car2);
+      userService.add(user3);
+      carService.add(car3);
+      userService.add(user4);
+      carService.add(car4);
+
+
+      List<User> users = userService.listUsers();
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = "+user.getCar().toString() + "\n");
+      }
+
+      System.out.println(userService.findByCar("Lada", 7));
+
+      context.close();
+   }
+}
